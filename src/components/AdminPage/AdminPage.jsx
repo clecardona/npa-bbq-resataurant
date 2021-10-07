736681825eba9e38268cd557ee964a2d9f714836
clecardona <clecardona@gmail.com>
2021-10-07 14:04:57 +0200

    adminpage refactored

--- src/components/AdminPage/AdminPage.jsx
+++ src/components/AdminPage/AdminPage.jsx
@@ -1,74 +1,37 @@
-import { useState, useEffect } from "react";
-import AddCategory from "./AddCategory";
-import { getCollection } from "../../scripts/fireStore";
+import { useState } from "react";
 import { getFirestore } from "firebase/firestore/lite";
+
+import useFetch from "../../hooks/useFetch";
+import AddCategory from "./AddCategory";
 import firebaseInstance from "../../scripts/firebase";
 import Sorter from "./Sorter";
 import AddMeal from "./AddMeal";
 
 export default function AdminPage() {
-  const [meals, setMeals] = useState([]);
-  const [categories, setCategories] = useState([]);
-  const [status, setStatus] = useState(0); // 0: loading, 1: loaded, 2: error
+  //hooks
   const [isCategory, setIsCategory] = useState(false);
-
+  const categories = useFetch("categories");
   // Properties
   const database = getFirestore(firebaseInstance);
 
   // Methods
-  useEffect(() => {
-    getCollection(database, "meals")
-      .then((result) => {
-        setMeals(result);
-        setStatus(1);
-      })
-      .catch((error) => {
-        console.log(error);
-        setStatus(2);
-      });
-  }, [database]);
-
-  useEffect(() => {
-    getCollection(database, "categories")
-      .then((result) => {
-        setCategories(result);
-        setStatus(1);
-      })
-      .catch((error) => {
-        console.log(error);
-        setStatus(2);
-      });
-  }, [database]);
-
-  //console.log(categories);
-
-  const Meals = meals.map((item) => {
-    return <p key={item.id}>{item.title}</p>;
-  });
-
-  const Categories = categories.map((item) => {
-    return <p key={item.id}>{item.name}</p>;
-  });
 
   return (
-    <main>
-      <div className="page-admin">
-        <section>
-          {status === 0 && <p>Loading ⏱</p>}
-          {status === 2 && <p>Error 🚨</p>}
-          {/* <h2>Categories</h2>
-        {status === 1 && <ul>{Categories}</ul>}
-
-        <h2>Meals</h2>
-        {status === 1 && <ul>{Meals}</ul>} */}
-        </section>
-        <Sorter hook={[isCategory, setIsCategory]} />
-        {isCategory ? (
-          <AddCategory categories={categories} database={database} />
-        ) : (
-          <AddMeal categories={categories} database={database} />
-        )}
-      </div>
-    </main>
+    <>
+      {categories.loading === true && <p>Loading ⏱</p>} {/* TODO - Spinner */}
+      {categories.error !== null && <p>Error 🚨</p>} {/* TODO - custom error */}
+      {!categories.loading && categories.error === null && (
+        <main>
+          <div className="page-admin">
+            <Sorter hook={[isCategory, setIsCategory]} />
+            {isCategory ? (
+              <AddCategory categories={categories.data} database={database} />
+            ) : (
+              <AddMeal categories={categories.data} database={database} />
+            )}
+          </div>
+        </main>
+      )}
+    </>
   );
 }
