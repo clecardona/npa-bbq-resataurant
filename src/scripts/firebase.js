bf86a6134a5f8660761ace5c0c054869963835df
clecardona <clecardona@gmail.com>
2021-10-15 13:12:49 +0200

    switch FB

--- src/scripts/firebase.js
+++ src/scripts/firebase.js
@@ -1,14 +1,23 @@
 import { initializeApp } from "firebase/app";
 
-const firebaseConfiguration = {
+/* const firebaseConfig= {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: "npa-bbq-restaurant.firebaseapp.com",
   projectId: "npa-bbq-restaurant",
   storageBucket: "npa-bbq-restaurant.appspot.com",
   messagingSenderId: "992711479795",
   appId: "1:992711479795:web:a446d85b325ba48c7bc286",
+}; */
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: "yet-bbq.firebaseapp.com",
+  projectId: "yet-bbq",
+  storageBucket: "yet-bbq.appspot.com",
+  messagingSenderId: "1086421258954",
+  appId: "1:1086421258954:web:00a450ec1d260dce935a33",
 };
 
-const firebaseInstance = initializeApp(firebaseConfiguration);
+const firebaseInstance = initializeApp(firebaseConfig);
 
 export default firebaseInstance;
